8d8621118fa864a8ae1ce1d0a7812a2c1088bd1d
VictoriaRe <victoriaregina98@gmail.com>
2016-11-07 22:23:10 +0300

    Add files via upload

---
+++ HW4/hw2 python var2.py
@@ -0,0 +1,12 @@
+s=0
+while s==0:
+    w=str(input("Введите слово..."))
+    for index, letter in enumerate(w):
+        n=index+1
+        if letter=="о" or letter=="п" or letter=="е":
+            if n%2!=0:
+                print (letter)
+                s=1
+  
+
+                
